709c1db2990fddd91fc0f6b7f30da65c4f532d85
Ninna994 <inanikolina@gmail.com>
2021-08-19 19:28:06 +0200

    Page Object Models

--- cypress/integration/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/integration/automation-test-store/add-multiple-items-to-basket.js
@@ -1,3 +1,6 @@
+import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO"
+import AutoStore_HairCare_PO from "../../support/pageObjects/automation-test-store/AutoStore_HairCare_PO"
+
 /// <reference types='Cypress' />
 
 describe('Add multiple items to basket', () => {
@@ -17,6 +20,28 @@ describe('Add multiple items to basket', () => {
             cy.addProductToBasket(element)
         })
         cy.get('.dropdown-toggle > .fa').click()
+    })
+
+
+})
+
+describe.only('Add multiple items to basket - Improved version', () => {
+
+    const autoStore_Homepage_PO = new AutoStore_Homepage_PO()
+    const autoStore_HairCare_PO = new AutoStore_HairCare_PO()
+
+    before(() => {
+        cy.fixture("products.json").then(function (data) {
+            globalThis.data = data
+        })
+    })
+    beforeEach(() => {
+        autoStore_Homepage_PO.accessHomepage()
+        autoStore_Homepage_PO.clickOn_HairCare_Link()
+    })
+
+    it("Add specific items to basket", () => {
+        autoStore_HairCare_PO.addHairCareProductsToBasket()
     })
 
 
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,4 +1,5 @@
 import Homepage_PO from '../../support/pageObjects/webdriver-uni/Homepage_PO'
+import Contact_Us_PO from '../../support/pageObjects/webdriver-uni/Contact_Us_PO'
 
 /// <reference types="Cypress" />
 
@@ -118,12 +119,17 @@ describe("Test Contact Us form via WebdriverUni", () => {
         // cy.get('[name="last_name"]').type(data.last_name)
         // cy.get('[name="email"]').type(data.email)
         // cy.get('[name="message"]').type(data.body)
-        cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, data.email, "How can I learn anything?", 'h1', "Thank You for your Message")
 
+        // support / commands.js
+        // cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, data.email, "How can I learn anything?", 'h1', "Thank You for your Message")
+
+        // support / pageObjects / Contact us PO approach
+        const contact_Us_PO = new Contact_Us_PO()
+        contact_Us_PO.contactForm_Submission(Cypress.env('first_name'), data.last_name, data.email, "How can I learn anything?", 'h1', "Thank You for your Message")
 
     })
 
-    it.only("HAPPY PATH - ENV variables insert data from fixtures", () => {
+    it("HAPPY PATH - ENV variables insert data from fixtures", () => {
 
         //cypress code
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
@@ -133,6 +139,12 @@ describe("Test Contact Us form via WebdriverUni", () => {
         // cy.get('[name="email"]').type(data.email)
         // cy.get('[name="message"]').type(data.body)
         cy.webdriverUni_ContactForm_Submission(Cypress.env('first_name'), data.last_name, data.email, "How can I learn anything?", 'h1', "Thank You for your Message")
+
+
+    })
+
+    it.only("SAD PATH - Object model - Should not be able to submit a successfull submission via contact us form  as all fields are required", () => {
+        cy.webdriverUni_ContactForm_Submission(Cypress.env('first_name'), data.last_name, "How can I learn anything?", 'body', "Error: Invalid email address")
 
 
     })
